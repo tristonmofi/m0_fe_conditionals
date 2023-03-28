31f80996bd4d19674eb2b993852de34278900171
Triston Modlin-Filippi <127793213+tristonmofi@users.noreply.github.com>
2023-03-27 20:31:08 -0600

    add first question answered

--- comparisons.js
+++ comparisons.js
@@ -23,6 +23,8 @@ console.log("Is numberTeachers greater than numberStudents?", numberTeachers > n
 // YOU DO: log the result of the comparison: is numberTeachers less than numberStudents?
 // this should log: true
 
+console.log("is numberTeachers less than numberStudents?", numberTeachers < numberStudents);
+
 // YOU DO: log the result of the comparison: is numberTeachers strictly equal to stringTeachers?
 // this should log: false
 
