fb44f72e17a41f1a75cd985d6e78613854c364a8
Triston Modlin-Filippi <127793213+tristonmofi@users.noreply.github.com>
2023-03-28 14:02:45 -0600

    add changes to consolelogs

--- comparisons.js
+++ comparisons.js
@@ -17,7 +17,9 @@ var stringTeachers = "4"
 var numberDogs = 0
 
 // EXAMPLE: log the result of the comparison: is numberTeachers greater than numberStudents?
+
 console.log("Is numberTeachers greater than numberStudents?", numberTeachers > numberStudents);
+
 // this should log: "Is numberTeachers greater than numberStudents?" false
 
 // YOU DO: log the result of the comparison: is numberTeachers less than numberStudents?
