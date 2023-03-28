02e44f0e2cae91b63063f45e3725b618688b3701
Triston Modlin-Filippi <127793213+tristonmofi@users.noreply.github.com>
2023-03-28 14:52:01 -0600

    Code challenge assignment

--- codeChallenges/challengeYourBrain.js
+++ codeChallenges/challengeYourBrain.js
@@ -1,6 +1,8 @@
-var goodDrivingRecord = true;
-var age = 24;
+var goodDrivingRecord = false;
+var age = 27;
 
 if (goodDrivingRecord == true && age >= 25) {
-    console.log("Driver gets a discount on the car rental");
-} 
+    console.log("The driver gets a discount on the car rental");
+} else {
+    console.log("The driver does not get a discount");
+}
